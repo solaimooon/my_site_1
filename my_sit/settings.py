2c1882ada0aace220881e3927b97d3093ef4d5a7
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-01-20 18:07:19 +0330

    add websit app in setting

--- my_sit/settings.py
+++ my_sit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'websit',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_sit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'websit/template']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
