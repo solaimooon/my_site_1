af099ee027031f36a0c2767f351dd5ba682da64d
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-01-30 00:52:41 +0330

    complite post model and register it to admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+
+
 class post (models.Model):
-    title=models.CharField(max_length=255)
-    detail=models.TextField()
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    published_date = models.DateTimeField(null=True)
+    def __str__(self):
+        return "{}_{}".format(self.id,self.title)
 # Create your models here.
