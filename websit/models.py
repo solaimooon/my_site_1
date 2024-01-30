d15cd6840ef02d7cc92274b7325afe53b3cf8fc1
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-01-30 16:44:01 +0330

    creat model for contacts

--- websit/models.py
+++ websit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class contacts (models.Model):
+    name=models.CharField(max_length=255)
+    email=models.EmailField()
+    subject = models.TextField()
+    message = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    update_time= models.TimeField(auto_now=True)
 # Create your models here.
