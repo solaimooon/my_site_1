389f126f1e12feccbdef545a8d77a8d0198dcd43
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-01-30 16:55:57 +0330

    customize admin for post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(post)
+
+class post_admin(admin.ModelAdmin):
+    list_display = ['title','counted_view','status','created_date','updated_date']
+    date_hierarchy = 'created_date'
+
+
+admin.site.register(post, post_admin)
 # Register your models here.
