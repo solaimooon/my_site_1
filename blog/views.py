466ad3761942bca4e2a66958ab11545b10f6f323
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-02-10 10:52:35 +0330

    configure the member of view and truncate the content in home blog

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,10 @@ def blog_veiw(request):
 def blog_detail_veiw(request, id):
     post_detail=post.objects.filter(pk=id, status=True)
     post_detail_notlist=post_detail[0]
+    counter =post_detail_notlist.counted_view
+    counter=counter+1
+    post_detail_notlist.counted_view=counter
+    post_detail_notlist.save()
     context={"post_detail_notlist_key":post_detail_notlist}
     return render(request, 'blog/blog-single.html',context)
 # Create your views here.
