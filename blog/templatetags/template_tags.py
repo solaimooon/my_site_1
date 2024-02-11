91fcad9437280a43951d0b5b9cd28ca10ba616f5
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-02-11 16:37:49 +0330

    just test simple - filter -inclusion teg

--- blog/templatetags/template_tags.py
+++ blog/templatetags/template_tags.py
@@ -1,7 +1,21 @@
 from django import template
+from blog.models import *
 register = template.Library()
 
+
 @register.simple_tag
-def hello ():
-    return "heksdg;sdgodsi"
+def simpel_template_tag ():
+    return "heksdg;sdgodsijkhjkhjkhjkhjkhjkjhkggkhlkjdroijgodijyoidjgiportoojktpohjd"
+
+
+@register.filter
+def contatnt_fileter(content,number):
+    return content[:20:]
+
+
+@register.inclusion_tag('blog/popular_post.html')
+def include_test():
+    posts=post.objects.all().order_by('published_date').values()
+    return {'posts':posts}
+
 
