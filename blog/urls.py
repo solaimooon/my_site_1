09a6529f1cbd1937657ce196b772494286632920
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-02-19 10:52:30 +0330

    setup search component

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',blog_veiw,name='blog_home'),
     path('category/<str:str>/',blog_veiw,name='category'),
     path('athour/<str:athour>',blog_veiw,name='athour'),
-    path('/<int:id>/',blog_detail_veiw,name='blog_detail'),
+    path('<int:id>/',blog_detail_veiw,name='blog_detail'),
+    path('search_post/',search_post,name='search_post'),
     path('test/',test_view)
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,31 +5,42 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
 
-def blog_veiw(request,str=None,athour=None):
+# we can use (str=None,athour=None) as parameter in blow function
+def blog_veiw(request, **kwargs):
     posts = post.objects.filter(status=1)
-    if (str):
-        posts = post.objects.filter(category__name=str, status=True)
-    elif athour:
-        posts=post.objects.raw("select p.* from auth_user a inner join پست p   on p.athour_id=a.id where a.username=%s",[athour])
+    if (kwargs.get("str") != None):
+        posts = post.objects.filter(category__name=kwargs[str], status=True)
+    elif (kwargs.get("aathour") != None):
+        posts = post.objects.raw(
+            "select p.* from auth_user a inner join پست p   on p.athour_id=a.id where a.username=%s",
+            [kwargs["athour"]])
+        # posts=post.objects.filter(athour__username=athour)
     context = {"posts_key": posts}
-    return render(request,'blog/blog-home.html',context)
+    return render(request, 'blog/blog-home.html', context)
 
 
 def blog_detail_veiw(request, id):
-    post_detail=post.objects.filter(pk=id, status=True)
-    post_detail_notlist=post_detail[0]
-    counter =post_detail_notlist.counted_view
-    counter=counter+1
-    post_detail_notlist.counted_view=counter
+    post_detail = post.objects.filter(pk=id, status=True)
+    post_detail_notlist = post_detail[0]
+    counter = post_detail_notlist.counted_view
+    counter = counter + 1
+    post_detail_notlist.counted_view = counter
     post_detail_notlist.save()
-    context={"post_detail_notlist_key":post_detail_notlist}
-    return render(request, 'blog/blog-single.html',context)
+    context = {"post_detail_notlist_key": post_detail_notlist}
+    return render(request, 'blog/blog-single.html', context)
 
 
-#def category_view(request,str):
+# def category_view(request,str):
 
+def search_post(request):
+    posts=post.objects.filter(content__contains=request.GET.get("search")) | post.objects.filter(title__contains=request.GET.get('search'))
+    context={"posts_key":posts}
+    return render(request,'blog/blog-home.html',context)
 
 
 def test_view(request):
-    return render(request,'test.html')
+    print(request.__dict__)
+    print(request.method)
+    request.GET.get()
+    return render(request, 'test.html')
 # Create your views here.
